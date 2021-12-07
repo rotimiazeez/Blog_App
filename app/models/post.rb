81b13bca1c645c257d184f28fc890469a029452a
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-12-07 07:53:15 -0800

    feat: Create model classes for all entities and setup association between models

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+has_many :posts, dependent: :destroy
+has_many :likes, dependent: :destroy
+has_many :comments, dependent: :destroy
+
+end
