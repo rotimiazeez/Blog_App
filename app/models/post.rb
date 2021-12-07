15e1c97dc316b5c36c34291dfb0a5ca74ee53ab3
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-12-07 08:00:32 -0800

    feat: Add a method which returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,11 @@ belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  
+  def increment_counter
+    user.update(posts_counter: user.posts_counter + 1)
+  end
+
+  def recent_comments(limit = 5)
+    comments.last(limit)
+  end
 end
