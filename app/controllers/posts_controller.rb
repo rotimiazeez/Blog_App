aa363f915f64f9a4d65265e630bcaa8a6ca6e7b9
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-12-11 10:34:30 -0800

    Add alias to model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:author_id])
+    @user = User.find(params[:id])
     @posts = @user.recent_posts
   end
 
   def show
-    @user = User.find(params[:author_id])
+    @user = User.find(params[:id])
     @post = @user.posts.find(params[:id])
     @comments = @post.comments.all
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, :class_name => :User,:foreign_key => "author_id"
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
