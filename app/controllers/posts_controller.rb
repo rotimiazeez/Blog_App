a2fbe47c94ac8652ebe9a7630ca5d300decad047
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-12-11 17:57:04 -0800

    feat: Find and fix N+1 queries problems

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,31 @@
 class PostsController < ApplicationController
+  def index; end
   def index
-    @user = User.find(params[:author_id])
+    @user = User.find(params[:user_id])
     @posts = @user.recent_posts
   end
 
+  def show; end
   def show
-    @user = User.find(params[:id])
-    @post = @user.posts.find(params[:id])
-    @comments = @post.comments.all
+    @user = User.find(params[:user_id])
+    @post = @user.posts.includes(:comments).find(params[:id])
+    @comments = @post.comments.all.order('created_at')
   end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
   def create
     post = current_user.posts.new(post_params)
 
     respond_to do |format|
       format.html do
-        redirect_to user_post_path(post.user.id, post.id) if post.save
+        if post.save
+          redirect_to user_post_path(post.user.id, post.id)
+        else
+          flash.now[:alert] = 'Error: post not published'
+        end
       end
     end
   end
