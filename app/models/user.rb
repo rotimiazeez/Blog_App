300f67bea2027f368bcd49b27c606dae4298192e
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-12-16 14:56:35 -0800

    feat: Implement user login and signup feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy, foreign_key: :author_id
   has_many :comments, dependent: :destroy, foreign_key: :author_id
   has_many :likes, dependent: :destroy, foreign_key: :author_id
 
   def recent_posts(limit = 3)
-    posts.last(limit)
+    posts.includes(:comments).order(created_at: :desc).limit(limit)
   end
 end
