9789c3f776cb60d61933dc17d77ecfbcc545e486
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-12-17 09:11:01 -0800

    Fix: Fix linter error

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,16 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   after_save :update_posts_counter
-  
+
   validates :title, presence: true,
-  length: { maximum: 250, too_long: 'must not exceed 250 characters' }
-validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+                    length: { maximum: 250, too_long: 'must not exceed 250 characters' }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-def update_posts_counter(value)
-user.update('posts_counter', value)
-end
+  def update_posts_counter(value)
+    user.update('posts_counter', value)
+  end
 
-def recent_comments(limit = 5)
-comments.order(created_at: :desc).limit(limit)
-end
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
